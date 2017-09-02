849cb74b4dff8e70e5434f45a86d7f4f622b9107
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2017-09-02 17:35:56 -0500

    Update essentials.js

--- essentials.js
+++ essentials.js
@@ -1,1 +1,92 @@
+var Button = android.widget.Button;
+var TV = android.widget.TextView;
+var LL = android.widget.LinearLayout;
+var View = android.view.View;
+var RL = android.widget.RelativeLayout;
+var SV = android.widget.ScrollView;
+var PW = android.widget.PopupWindow;
+var Runnable = java.lang.Runnable;
 
+var GUI;
+var menu;
+var exitUI;
+
+function dip2px(dips){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
+}
+
+function newLevel(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new Runnable({ run: function(){
+        try{
+            var layout = new LL(ctx);
+            layout.setOrientation(1);
+
+            var menuBtn = new Button(ctx);
+            menuBtn.setText("Menu");
+            menuBtn.setOnClickListener(new View.OnClickListener({
+                onClick: function(viewarg){
+                    mainMenu();
+                }
+            }));
+            layout.addView(menuBtn);
+
+            GUI = new PW(layout, RL.LayoutParams.WRAP_CONTENT, RL.LayoutParams.WRAP_CONTENT);
+            GUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+            GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.BOTTOM, 0, 0);
+        }catch(err){
+            print("An error occured: " + err);
+        }
+    }}));
+}
+
+function mainMenu(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
+        try{
+            var menuLayout = new LL(ctx);
+            var menuScroll = new SV(ctx);
+            var menuLayout1 = new LL(ctx);
+            menuLayout.setOrientation(1);
+            menuLayout1.setOrientation(1);
+
+            menuScroll.addView(menuLayout);
+            menuLayout1.addView(menuScroll);
+          
+            //Buttons
+            
+            menu = new android.widget.PopupWindow(menuLayout1, ctx.getWindowManager().getDefaultDisplay().getWidth()/2, ctx.getWindowManager().getDefaultDisplay().getHeight());
+            menu.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.BLACK));
+            menu.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
+        }catch(error){
+            print("An error occured: " + error);
+        }
+    }}));
+}
+
+function exit(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new Runnable({ run: function(){
+        try{
+            var xLayout = new LL(ctx);
+			
+            var xButton = new Button(ctx);
+            xButton.setText("x");
+            xButton.setTextColor(android.graphics.Color.WHITE);
+            xButton.setOnClickListener(new View.OnClickListener({
+                onClick: function(viewarg){
+                    exitUI.dismiss();
+                    menu.dismiss();
+                }
+            }));
+            xLayout.addView(xButton);
+			
+            exitUI = new PW(xLayout, dip2px(40), dip2px(40));
+            exitUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+            exitUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
+        }catch(exception){
+            print(exception);
+        }
+    }}));
+}
